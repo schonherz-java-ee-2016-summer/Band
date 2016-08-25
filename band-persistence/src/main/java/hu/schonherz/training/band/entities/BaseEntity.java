316929ffb6f64ca4533b6c708e21d7005215b28f
Veress Armin <veress.armin@gmail.com>
2016-08-25 09:35:28 +0200

    4-Created a new branch

---
+++ band-persistence/src/main/java/hu/schonherz/training/band/entities/BaseEntity.java
@@ -0,0 +1,15 @@
+package hu.schonherz.training.band.entities;
+
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+/**
+ * Created by Lenovo on 2016.08.25..
+ */
+
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+
+
+}
