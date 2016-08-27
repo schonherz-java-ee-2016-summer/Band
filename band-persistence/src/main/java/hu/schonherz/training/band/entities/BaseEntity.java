196cce4a62ffdcfd29b4ab03cd4e7a1bebb1cc43
Veress Armin <veress.armin@gmail.com>
2016-08-27 11:03:07 +0200

    Changing may BaseEntity class to match it to the others

--- band-persistence/src/main/java/hu/schonherz/training/band/entities/BaseEntity.java
+++ band-persistence/src/main/java/hu/schonherz/training/band/entities/BaseEntity.java
@@ -1,26 +1,24 @@
 package hu.schonherz.training.band.entities;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
- * Created by Lenovo on 2016.08.25..
+ * Created by Attila on 2016.08.25..
  */
+
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public int id;
+    private Long id;
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 }
