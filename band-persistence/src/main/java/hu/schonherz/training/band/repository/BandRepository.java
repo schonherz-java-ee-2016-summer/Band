67b576dc23ba4cc051c579bd8559abde24d7d068
BarocsiNorbert <barocsinorbert@gmail.com>
2016-08-28 09:54:52 +0200

    Implemented BandRepository interface.

--- band-persistence/src/main/java/hu/schonherz/training/band/repository/BandRepository.java
+++ band-persistence/src/main/java/hu/schonherz/training/band/repository/BandRepository.java
@@ -1,8 +1,24 @@
 package hu.schonherz.training.band.repository;
 
+import hu.schonherz.training.band.entities.Band;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+
 /**
  * Created by Mindfield on 2016. 08. 28..
  */
-public interface BandRepository {
+@Repository
+@Transactional(propagation = Propagation.REQUIRED)
+public interface BandRepository extends JpaRepository<Band, Long>{
+
+    Band findById(Long id);
+
+    Band findBandByName(String name);
+
+    Collection<Band> findAllBand();
     
 }
