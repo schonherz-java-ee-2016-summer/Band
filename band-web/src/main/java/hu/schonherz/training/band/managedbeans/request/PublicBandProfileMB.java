fdb82a0980f131ac7fdda2ecb1f7c790f8076587
BarocsiNorbert <barocsinorbert@gmail.com>
2016-09-09 08:43:54 +0200

    24 - Band public profile view is working.

---
+++ band-web/src/main/java/hu/schonherz/training/band/managedbeans/request/PublicBandProfileMB.java
@@ -0,0 +1,49 @@
+package hu.schonherz.training.band.managedbeans.request;
+
+import hu.schonherz.training.band.managedbeans.view.BandMB;
+import hu.schonherz.training.band.service.BandService;
+import hu.schonherz.training.band.vo.BandVo;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ */
+@ManagedBean(name = "publicBandProfileBean")
+@RequestScoped
+public class PublicBandProfileMB {
+
+    @ManagedProperty("#{bandBean}")
+    private BandMB bandMB;
+
+    @EJB
+    private BandService bandService;
+
+    public void onload() {
+        if (!FacesContext.getCurrentInstance().isPostback()) {
+            BandVo bandVo = bandService.getBandById(bandMB.getBandVo().getId());
+
+            bandMB.setBandVo(bandVo);
+        }
+    }
+
+    public BandMB getBandMB() {
+        return bandMB;
+    }
+
+    public void setBandMB(BandMB bandMB) {
+        this.bandMB = bandMB;
+    }
+
+    public BandService getBandService() {
+        return bandService;
+    }
+
+    public void setBandService(BandService bandService) {
+        this.bandService = bandService;
+    }
+}
