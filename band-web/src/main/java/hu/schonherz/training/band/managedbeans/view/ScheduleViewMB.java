11e1a3c89eef1a6c57a74044e3e38be92dd5f4c2
Veress Armin <veress.armin@gmail.com>
2016-09-15 21:29:32 +0200

    32-calendar-on-the-public-profile

--- band-web/src/main/java/hu/schonherz/training/band/managedbeans/view/ScheduleViewMB.java
+++ band-web/src/main/java/hu/schonherz/training/band/managedbeans/view/ScheduleViewMB.java
@@ -2,17 +2,10 @@ package hu.schonherz.training.band.managedbeans.view;
 
 
 import java.io.Serializable;
-import java.util.Date;
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
 
-import org.primefaces.event.ScheduleEntryMoveEvent;
-import org.primefaces.event.ScheduleEntryResizeEvent;
-import org.primefaces.event.SelectEvent;
 import org.primefaces.model.DefaultScheduleEvent;
 import org.primefaces.model.DefaultScheduleModel;
 import org.primefaces.model.ScheduleEvent;
@@ -44,41 +37,5 @@ public class ScheduleViewMB implements Serializable {
 
     public void setEvent(ScheduleEvent event) {
         this.event = event;
-    }
-
-    public void addEvent(ActionEvent actionEvent) {
-        if (event.getId() == null) {
-            eventModel.addEvent(event);
-        } else {
-            eventModel.updateEvent(event);
-        }
-
-        event = new DefaultScheduleEvent();
-    }
-
-    public void onEventSelect(SelectEvent selectEvent) {
-        event = (ScheduleEvent) selectEvent.getObject();
-    }
-
-    public void onDateSelect(SelectEvent selectEvent) {
-        event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
-    }
-
-    public void onEventMove(ScheduleEntryMoveEvent event) {
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:" +
-                event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
-
-        addMessage(message);
-    }
-
-    public void onEventResize(ScheduleEntryResizeEvent event) {
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:" +
-                event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
-
-        addMessage(message);
-    }
-
-    private void addMessage(FacesMessage message) {
-        FacesContext.getCurrentInstance().addMessage(null, message);
     }
 }
