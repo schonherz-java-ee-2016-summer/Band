5ed1626a73c3b2cd8b41b15eb0e63779db233ee4
BarocsiNorbert <barocsinorbert@gmail.com>
2016-09-19 14:56:38 +0200

    30 - Implemented remote service method.

--- band-service-client-api/src/main/java/hu/schonherz/training/band/ejb/remote/stateless/BandRemoteService.java
+++ band-service-client-api/src/main/java/hu/schonherz/training/band/ejb/remote/stateless/BandRemoteService.java
@@ -1,10 +1,15 @@
 package hu.schonherz.training.band.ejb.remote.stateless;
 
+import hu.schonherz.training.band.vo.BandVo;
+
+import java.util.List;
+
 /**
  * Created by Mindfield on 2016. 09. 14..
  */
 public interface BandRemoteService {
 
-    String getAllBands();
+    List<BandVo> getAllBands();
 
+    List<BandVo> getBandsWithPaging(int page, int pageSize);
 }
